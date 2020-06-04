5a73fba1d9300ac34f8e1cdd208e168d4067d723
brody sullivan <brodysullivan0@gmail.com>
2020-06-04 20:57:22 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,7 +32,7 @@ def adding
                   
                  }
                  shipping_manifest["muskets"] = 2 
-                 shipping_manifest["gun powder"] = 4
+                 
 
 
 
